4a85cdb4477804e1381b98fb478f836594d75a5e
nm <nagamocha@gmail.com>
2024-02-27 11:12:58 +0300

    Implement simple fuzzy search

--- examples/usage.py
+++ examples/usage.py
@@ -5,11 +5,15 @@ from polars_fuzzy_match import fuzzy_match
 def main():
     df = pl.DataFrame(
         {
-            "strs": ["foo", "barz", "quz", "foobar", "for"],
+            "strs": ["foo", "barz", "quz", "barfoo", "foro"],
         }
     )
     needle = "foo"
-    out = df.with_columns(score=fuzzy_match(pl.col("strs"), needle))
+    out = (
+        df.with_columns(score=fuzzy_match(pl.col("strs"), needle))
+        .filter(pl.col("score").is_not_null())
+        .sort(by="score", descending=True)
+    )
     print(out)
 
 
--- src/fuzzy_match.rs
+++ src/fuzzy_match.rs
@@ -1,28 +1,32 @@
 use polars::prelude::*;
 use pyo3_polars::derive::polars_expr;
 use pyo3_polars::export::polars_core::utils::CustomIterTools;
+
+use nucleo::Utf32Str;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct FuzzyMatchKwargs {
     needle: String,
 }
 
-// exact match
-#[polars_expr(output_type=Boolean)]
+#[polars_expr(output_type=UInt32)]
 fn fuzzy_match(haystack: &[Series], kwargs: FuzzyMatchKwargs) -> PolarsResult<Series> {
     let ca = (&haystack[0]).str()?;
-    let needle = kwargs.needle;
-    let out: BooleanChunked = ca
+
+    let mut nucleo = nucleo::Matcher::new(nucleo::Config::DEFAULT.match_paths());
+    let needle = Utf32Str::Ascii(kwargs.needle.as_bytes());
+    let mut buf = Vec::new();
+    let out: UInt32Chunked = ca
         .into_iter()
         .map(|v: Option<&str>| {
             if let Some(s) = v {
-                Some(s == &needle)
+                let haystack = Utf32Str::new(s, &mut buf);
+                nucleo.fuzzy_match(haystack, needle).map(|v| v as u32)
             } else {
                 None
             }
         })
         .collect_trusted();
-
     Ok(out.into_series())
 }
